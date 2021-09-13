cd10d0715aaba9562928787e13a117ecb24a4eed
Richmond Ndigwe <49767574+richcode43@users.noreply.github.com>
2021-09-14 00:36:36 +0100

    Add files via upload

--- DateObject/JS/date.js
+++ DateObject/JS/date.js
@@ -59,7 +59,35 @@ if (exitDate === presentDate) {
 //     console.log(days);
 //     document.getElementById("msg").innerText = `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`;
 //     if (diff < 0) {
-//         clearTimeout(countDown);
+//         clearInterval(countDown);
 //         alert("time has expired");
 //     }
 // }, 1000);
+
+var countDownDate = new Date("sep 14, 2021 00:40").getTime();
+
+// Update the count down every 1 second
+let count = setInterval(function() {
+
+    // Get today's date and time
+    let now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    let diff = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="demo"
+    document.getElementById("demo").innerHTML = days + "d " + hours + "h " +
+        minutes + "m " + seconds + "s ";
+
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(count);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
